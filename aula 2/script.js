b1806e59f3910873db93eb214445ff6053bbdf8c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 22:22:19 -0300

    aula 2

---
+++ aula 2/script.js
@@ -0,0 +1,89 @@
+/*
+let idade = 16;
+let temCarteira = false;
+
+// AND (&&) - ambas condições devem ser verdadeiras
+if (idade >= 18 && temCarteira) {
+  console.log("Pode dirigir");
+}
+
+// OR (||) - pelo menos uma condição deve ser verdadeira
+if (idade < 18 || !temCarteira) {
+  console.log("Não pode dirigir");
+}
+
+// NOT (!) - inverte o valor booleano
+if (!temCarteira) {
+  console.log("Precisa de carteira");
+}
+let tarefa = true;
+
+if(tarefa) {
+    console.log('tarefa concluida')
+}
+else{
+    console.log('tarefa n concluida')
+}
+
+let prioridade = 4;
+
+if(prioridade === 3) {
+    console.log('prioridade alta')
+}
+else if (prioridade === 2) {
+    console.log('prioridade média')
+}
+else if (prioridade === 1) {
+    console.log('prioridade baixa')
+}
+else if (prioridade === 4) {
+    console.log('prioridade altissima')
+}
+else {
+    console.log('valor incorreto')
+}
+
+let diaSemana = new Date().getDay();
+
+switch (diaSemana) {
+    case 0:
+        console.log('domingo');
+        break;
+    case 1:
+        console.log('segunda');
+        break;
+    case 2:
+        console.log('terça');
+        break;
+    case 3:
+        console.log('quarta');
+        break;
+    case 4:
+        console.log('quinta');
+        break;
+    case 5:
+        console.log('sexta');
+        break;
+    case 6:
+        console.log('sabado');
+        break;
+    default:
+        console.log('dia da semana invalido');
+        break;
+}
+console.log(diaSemana)
+
+for (let i=0;i<5;i++){
+    console.log('contagem: ' + i)
+}
+
+i = 0
+
+while (i<5) {
+    console.log('Contagem: ' + i)
+    i++
+}
+    
+*/
+
+
